c4b0c1be555a223f4a7e3ff2ee2051d7ddc7871d
loy-shalom <93376919+loy-shalom@users.noreply.github.com>
2022-06-01 20:41:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -35,3 +35,105 @@ fabric.Image.fromURL(get_image, function(Img){
     canvas.add(block_image_object);
 });
 }
+window.addEventListener("keydown", my_keydown);
+function my_keydown(e)
+{
+    keypressed=e.keyCode;
+    console.log(keypressed);
+    if(e.shiftKey==true && keypressed=='80')
+{
+    console.log("p and shift pressed together");
+    block_image_width=block_image_width+10;
+    block_image_height=block_image_height+10;
+    document.getElementById("current_width").innerHTML=block_image_width;
+    document.getElementById("current_height").innerHTML=block_image_height;
+}
+
+if(e.shiftKey==true && keypressed=='77')
+{
+    console.log("m and shift pressed together");
+    block_image_width=block_image_width-10;
+    block_image_height=block_image_height-10;
+    document.getElementById("current_width").innerHTML=block_image_width;
+    document.getElementById("current_height").innerHTML=block_image_height;
+    
+}
+
+if(keypressed=='38')
+{
+    up();
+    console.log("up");
+}
+
+if(keypressed=='40')
+{
+    down();
+    console.log("down");
+}
+
+if(keypressed=='37')
+{
+    left();
+    console.log("left");
+}
+
+if(keypressed=='39')
+{
+    right();
+    console.log("right");
+}
+
+if(keypressed=='87')
+{
+    new_image('wall.jpg');
+    console.log("w");
+}
+
+if(keypressed=='71')
+{
+    new_image('ground.png');
+    console.log("g");
+}
+
+if(keypressed=='76')
+{
+    new_image('light_green.png');
+    console.log("l");
+}
+
+if(keypressed=='84')
+{
+    new_image('trunk.jpg');
+    console.log("t");
+}
+
+if(keypressed=='82')
+{
+    new_image('roof.jpg');
+    console.log("r");
+}
+
+if(keypressed=='89')
+{
+    new_image('yellow_wall.png');
+    console.log("y");
+}
+
+if(keypressed=='68')
+{
+    new_image('dark_green.png');
+    console.log("d");
+}
+
+if(keypressed=='85')
+{
+    new_image('unique.png');
+    console.log("u");
+}
+
+if(keypressed=='67')
+{
+    new_image('cloud.jpg');
+    console.log("c");
+}
+}
