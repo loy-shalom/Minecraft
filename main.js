1d58ed89298ef41f939845476ffd0523963075df
loy-shalom <93376919+loy-shalom@users.noreply.github.com>
2022-06-03 20:33:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -137,3 +137,51 @@ if(keypressed=='67')
     console.log("c");
 }
 }
+
+function up()
+{
+    if(player_y>=0)
+    {
+        player_y=player_y-block_image_height;
+        console.log("block image height= "+block_image_height);
+        console.log("when up arrow key is pressed, x= "+player_x+" and y= "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function down()
+{
+    if(player_y<=500)
+    {
+        player_y=player_y+block_image_height;
+        console.log("block image height= "+block_image_height);
+        console.log("when down arrow key is pressed, x= "+player_x+" and y= "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function left()
+{
+    if(player_x>=0)
+    {
+        player_x=player_x-block_image_width;
+        console.log("block image width= "+block_image_width);
+        console.log("when left arrow key is pressed, x= "+player_x+" and y= "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function right()
+{
+    if(player_x<=850)
+    {
+        player_x=player_x+block_image_width;
+        console.log("block image width= "+block_image_width);
+        console.log("when right arrow key is pressed, x= "+player_x+" and y= "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
